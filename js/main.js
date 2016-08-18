899c677846c01a3b66835baeafe896d9f5e3a3b7
romainberger <romain@romainberger.com>
2016-08-18 16:08:44 +0200

    Add 404 page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 !function() {
   setTimeout(function() {
     var bg = document.querySelector('.home-bg')
-    if (bg) {
+    if (bg && !bg.hasClass('nope')) {
       bg.className += ' loaded'
     }
   }, 100)
